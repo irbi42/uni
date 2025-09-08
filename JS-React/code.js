ffeeeb32a7b0bf1b952bde3a62dbbabc61ce94ea
irbi42 <uschakowa23.05@gmail.com>
2025-09-08 15:03:57 +0700

    ex1p1 done

--- JS-React/code.js
+++ JS-React/code.js
@@ -1,1 +1,26 @@
 console.log("Hello!");
+
+function analyzeUser(user) {
+    let isadmin = "Нет";
+    if (user.isAdmin) {
+        isadmin = "Да";
+    };
+    var result = "Пользователь: " + user.name + ", возраст: " + user.age + ". Администратор: "+ isadmin + ". Увлечения: " + user.hobbies + ".";
+    return result;
+}
+
+const user = {
+    name: "Tom",
+    age: 12,
+    isAdmin: false,
+    hobbies: ["Видеоигры", "Рисование"]
+};
+
+const admin = {
+    name: "Bob",
+    age: 22,
+    isAdmin: true,
+    hobbies: ["Видеоигры", "Программирование", "вязание"]
+};
+console.log(analyzeUser(user));
+console.log(analyzeUser(admin));
